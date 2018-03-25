c128fbd33d11ed42f30d4796ab0abd8dff239207
mike <mikeposner@163.com>
2018-03-25 11:50:55 +0800

    跟路由设定及相关测试编写

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,4 +25,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "about | #{@base_title}"
   end
 
+  test "项目跟路径" do
+    get root_url
+    assert_response :success
+  end
+
 end
