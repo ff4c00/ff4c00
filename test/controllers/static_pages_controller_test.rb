b891daf44e857215470fc515ecdbf2fd4eafa8a9
mike <mikeposner@163.com>
2018-03-27 22:14:22 +0800

    替换为具名路由

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,25 +8,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get home_path
     assert_response :success
     assert_select "title", "home | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "help | #{@base_title}"
   end
 
   test "关于about页面的测试" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "about | #{@base_title}"
   end
 
   test "项目跟路径" do
-    get root_url
+    get root_path
     assert_response :success
   end
 
